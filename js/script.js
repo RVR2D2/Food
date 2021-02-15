c637125f33787c25e68f5e6cec41629d7736d9ef
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2021-02-15 13:27:18 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2020-10-06');
+    timer('.timer', '2021-10-06');
     cards();
     calc();
     forms('form', modalTimerId);
